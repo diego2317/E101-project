ba293dd1c8d3316f2541c4d12faa8c56da462947
diego2317 <diegokweiss@gmail.com>
2025-11-12 14:11:18 -0800

    setup

---
+++ hw9.m
@@ -0,0 +1,4 @@
+%% 1) Find the amplitude A and mean value 𝑎0 of the sampled pulse train data. Estimate the
+% period of the signal T0 (in CT) by inspecting the time domain samples. Note that this is a
+% rough estimate, since the true period may not be an integer multiple of the sampling rate.
+
